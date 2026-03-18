969cbb585529586b6e0c4ad63fc03612da90ee27
Phú Văn <nguyenvan6596@gmail.com>
2026-03-18 11:03:23 +0700

    edit requestmapping api vnpay

--- src/main/java/com/ecommerce/orderservice/api/PaymentApi.java
+++ src/main/java/com/ecommerce/orderservice/api/PaymentApi.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Collections;
 
 @RestController
-@RequestMapping("/api/payment")
+@RequestMapping("/api/orders/payment")
 public class PaymentApi {
 
     @Autowired
